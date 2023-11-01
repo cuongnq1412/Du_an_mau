d8ca8fa5d1495d9319cdf96e14133d836b649ea5
Nguyen Quang Cuong <nguyenquangcuong25022004@gmail.com>
2023-11-02 00:29:36 +0700

    lan1

--- admin/demo.php
+++
@@ -1,8 +0,0 @@
-<textarea id="editor" class="addcmt" required></textarea><br /><br />
-<script src="https://cdn.ckeditor.com/ckeditor5/40.0.0/classic/ckeditor.js"></script>
-
-<script>
-  ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
-    console.error(error);
-  });
-</script>
